46978cf5f1cc7d403e3a4f53fbc58978035bbfe0
klinok9 <0Dbhecyfqlty>
2020-10-31 09:43:52 +0300

    добавил 2 главу,изменил название

---
+++ Глава 2.Сортировка выбором/Selection_sort.py
@@ -0,0 +1,19 @@
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest_index = i
+            smallest = arr[i]
+    return smallest_index
+
+
+def selection_sort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print(selection_sort([10, 2, 7, 8, 11]))
